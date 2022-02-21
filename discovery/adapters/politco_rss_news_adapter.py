86b8dcc36ccef59cf67948b18b5e183c1ad8048f
DeejayRevok <seryi_one@hotmail.com>
2022-02-21 20:09:23 +0100

    Add politico adapter Add politico fetch definition Add politico adapter tests

---
+++ discovery/adapters/politco_rss_news_adapter.py
@@ -0,0 +1,55 @@
+from time import mktime, strptime
+import requests
+from xml.etree.ElementTree import Element, fromstring, tostring
+from typing import ClassVar, Iterator, Optional
+
+from bs4 import BeautifulSoup
+from lxml import html
+from xmltodict import parse
+from news_service_lib.models.new import New
+from news_service_lib.models.language import Language
+
+from discovery.adapters.source_adapter import SourceAdapter
+from log_config import get_logger
+
+LOGGER = get_logger()
+
+
+class PoliticoRSSNewsAdapter(SourceAdapter):
+    __DATE_FORMAT: ClassVar[str] = "%a, %d %b %Y %H:%M:%S EST"
+    __ROOT_NEW_TAG: ClassVar[str] = "item"
+
+    def _fetch(self) -> Iterator[Element]:
+        LOGGER.info("Fetching news from %s", self.source_params["politico_rss"])
+        response = requests.get(self.source_params["politico_rss"])
+        rss = fromstring(response.content)
+        for channel in rss:
+            for element in channel:
+                if element.tag == self.__ROOT_NEW_TAG:
+                    yield element
+
+    def _adapt_single(self, item: Element) -> Optional[New]:
+        raw_new_dict = parse(tostring(item).decode(), attr_prefix="")[self.__ROOT_NEW_TAG]
+        LOGGER.info("Found new with title %s", raw_new_dict["title"])
+
+        if "ns3:encoded" not in raw_new_dict:
+            return None
+
+        content = self.__parse_content(raw_new_dict["ns3:encoded"])
+        date = mktime(strptime(raw_new_dict["pubDate"], self.__DATE_FORMAT))
+
+        return New(
+            title=raw_new_dict["title"],
+            url=raw_new_dict["link"],
+            content=content,
+            source="Politico",
+            date=date,
+            language=Language.ENGLISH.value,
+        )
+
+    def __parse_content(self, html_string: str) -> str:
+        if html.fromstring(html_string).find(".//*") is not None:
+            html_content = BeautifulSoup(html_string, "html.parser").text
+            return self.__parse_content(html_content)
+        else:
+            return html_string
--- tests/unit/adapters/test_politico_rss_news_adapter.py
+++ tests/unit/adapters/test_politico_rss_news_adapter.py
@@ -5,14 +5,13 @@ from requests import Response
 from unittest import TestCase
 from unittest.mock import patch, Mock
 
+from discovery.adapters.politco_rss_news_adapter import PoliticoRSSNewsAdapter
 from news_service_lib.models.language import Language
 from news_service_lib.models.new import New
 
-from discovery.adapters.cbs_san_francisco_rss_news_adapter import CBSSanFranciscoRSSNewsAdapter
 
-
-class TestCBSSanFranciscoAdapter(TestCase):
-    XML_RESPONSE_PATH = join(dirname(dirname(__file__)), "resources", "cbs_san_francisco_news_response.xml")
+class TestPoliticoAdapter(TestCase):
+    XML_RESPONSE_PATH = join(dirname(dirname(__file__)), "resources", "politico_news_response.xml")
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -23,7 +22,7 @@ class TestCBSSanFranciscoAdapter(TestCase):
         self.mocked_rss_response = Mock(spec=Response)
         self.mocked_rss_response.content = self.test_rss_content
         self.test_rss_url = "test_rss_url"
-        self.adapter = CBSSanFranciscoRSSNewsAdapter({"cbs_sf_rss": self.test_rss_url})
+        self.adapter = PoliticoRSSNewsAdapter({"politico_rss": self.test_rss_url})
 
     @patch("requests.get")
     def test_fetch(self, requests_get):
@@ -31,12 +30,12 @@ class TestCBSSanFranciscoAdapter(TestCase):
 
         fetch_return = list(self.adapter.fetch())
 
-        expected_date = mktime(datetime(year=2022, month=2, day=2, hour=17, minute=46, second=36).timetuple())
+        expected_date = mktime(datetime(year=2022, month=2, day=21, hour=7, minute=00, second=00).timetuple())
         expected_new = New(
             title="test_title",
             url="test_link",
             content="test_content",
-            source="CBS San Francisco",
+            source="Politico",
             date=expected_date,
             language=Language.ENGLISH.value,
         )
